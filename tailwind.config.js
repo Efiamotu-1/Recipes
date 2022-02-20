3211526c313eb83181407ea68e0199832bec9f06
Efiamotu-1 <musahabeebefy10@gmail.com>
2022-02-20 06:38:17 +0100

    commit-one

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily : {
+        body : ['Nunito']
+      },
+
+      colors : {
+        primary : '#FF6363',
+        secondary : {
+          100 : '#E2E2D5',
+          200 : '#888883'
+        }
+        
+      }
+    }
+    
+  },
+  plugins: [],
+}
